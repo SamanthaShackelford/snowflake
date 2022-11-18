a8497bd11c57728391c0dcd7f52022b6ddbe5723
SamanthaShackelford <samantha@echobind.com>
2022-11-18 15:17:35 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { useRef, useEffect } from "react"
+import { Canvas, useThree } from "@react-three/fiber";
+import { OrbitControls, useHelper} from "@react-three/drei";
+import { PointLight, PointLightHelper } from "three"
+import { Snowflake } from './Snowflake'
+import { SkyBox } from './SkyBox'
+import state from './store'
 import "./App.css";
 
+
 function ThreeScene() {
+  const lightRef = useRef<PointLight>(null!);
+  useHelper(lightRef, PointLightHelper, 1, "red");
   return (
     <>
-      <ambientLight />
-      <pointLight position={[5, 5, 5]} intensity={1} />
-      <pointLight position={[-3, -3, 2]} />
+    {/* <ambientLight/> */}
+      {/* <pointLight ref={lightRef} position={[5, 5, 5]} intensity={4} /> */}
       <OrbitControls />
+      <Snowflake />
+      <SkyBox />
     </>
   );
 }
 
 function App() {
+  const scrollArea = useRef();
+  const onScroll = (e:any) =>{
+    console.log(e)
+    return (state.top.current = e.target.scrollTop)
+  }
+
+  // useEffect(() => void onScroll({target: scrollArea.current}), [])
+
   return (
     <div className="App h-screen">
-      <Canvas>
+      <Canvas 
+      // orthographic 
+        camera={{ position: [0, 0, 5] }}
+        // camera={{ fov: 35, zoom: 1.3, near: 1, far: 1000 }}
+      >
         <ThreeScene />
       </Canvas>
+      {/* <div ref={scrollArea} onScroll={onScroll}>
+          <div style={{ height: `${state.pages * 100}vh`}}></div>
+      </div> */}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/SkyBox.tsx
@@ -0,0 +1,10 @@
+import { useThree } from "@react-three/fiber";
+import { CubeTextureLoader } from "three";
+
+export function SkyBox() {
+  const { scene } = useThree();
+  const loader = new CubeTextureLoader();
+  const texture = loader.load(["sky-cube-map/px.png", "sky-cube-map/nx.png", "sky-cube-map/py.png", "sky-cube-map/ny.png", "sky-cube-map/pz.png", "sky-cube-map/nz.png"])
+  scene.background = texture;
+  return null
+}
