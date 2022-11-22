a5c1d0d55f30cb7b30520fdab43280290f968482
SamanthaShackelford <samantha@echobind.com>
2022-11-22 15:03:20 -0500

    tsx -> jsx

--- src/components/SkyBox.jsx
+++ src/components/SkyBox.jsx
@@ -6,5 +6,6 @@ export function SkyBox() {
   const loader = new CubeTextureLoader();
   const texture = loader.load(["sky-cube-map/px.png", "sky-cube-map/nx.png", "sky-cube-map/py.png", "sky-cube-map/ny.png", "sky-cube-map/pz.png", "sky-cube-map/nz.png"])
   scene.background = texture;
+  
   return null
 }
